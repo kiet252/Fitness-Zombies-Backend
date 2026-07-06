68dbc399e6792c6395dc2734e05ef9fb9ba7c966
kieta252 <caok104@gmail.com>
2026-07-06 16:02:01 +0700

    second commit, initialize project

---
+++ src/main/java/com/example/fitness_zombie_backend/FitnessZombieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitness_zombie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessZombieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessZombieBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitness_zombie_backend/FitnessZombieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitness_zombie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessZombieBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
